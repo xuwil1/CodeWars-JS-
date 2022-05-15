87a81f157ad70f4017bae5f7a36be4c65f3cef87
xuwil1 <73079502+xuwil1@users.noreply.github.com>
2022-05-14 20:12:20 -0400

    Create Convert_boolean_values_to_strings _Yes_or_No.java

---
+++ 7th Kyu/Convert_boolean_values_to_strings _Yes_or_No.java
@@ -0,0 +1,16 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    if(b==true){
+      return "Yes";
+    }
+    else{
+      return  "No";
+    }
+  } 
+}
