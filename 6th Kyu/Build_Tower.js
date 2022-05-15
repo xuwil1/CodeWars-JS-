b487f21cb798cdb6df33d94662e55851a85bba7a
xuwil1 <73079502+xuwil1@users.noreply.github.com>
2022-05-14 21:17:55 -0400

    Update Build_Tower.js

--- 6th Kyu/Build_Tower.js
+++ 6th Kyu/Build_Tower.js
@@ -22,18 +22,22 @@ And a tower with 6 floors looks like this:
 */
 
 function towerBuilder(nFloors) {
-  // build here
+  //create temporary array to hold tower
   let temp=[];
   for(let i=0; i<nFloors;i++){
+    //create temporary string to hold tower blocks of floor 
     let s="";
+    //create spaces on left side of tower block(s)
     s=space(s,(nFloors-(i+1)));
+    //create tower blocks on appropriate floor
     s=stars(s,(2*i+1));
+    //create spaces on left side of tower block(s)
     s=space(s,(nFloors-(i+1)));
     temp.push(s);
   }
   return temp;
 }
-
+//helper function to create spaces
 function space(s,n){
   while(n>0){
     s+=" ";
@@ -41,6 +45,7 @@ function space(s,n){
   }
   return s;
 }
+//helper function to create stars 
 function stars(s,n){
   while(n>0){
     s+="*";
