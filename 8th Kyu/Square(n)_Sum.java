9f07bbdab30c48fd480e78c0d3ca2484270af86f
xuwil1 <73079502+xuwil1@users.noreply.github.com>
2022-05-14 20:08:41 -0400

    Update Square(n)_Sum.java

--- 8th Kyu/Square(n)_Sum.java
+++ 8th Kyu/Square(n)_Sum.java
@@ -1,1 +1,15 @@
-
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
+public class Kata
+ {
+  public static int squareSum(int[] n)
+  { 
+   int sum=0;
+    for(int i=0;i<n.length;i++){
+      sum+=n[i]*n[i];
+    }
+    return sum;
+  }
+ }
