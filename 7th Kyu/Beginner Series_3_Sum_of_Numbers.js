706e8f16ed48e92a49080ddc45260371eb0d79e3
xuwil1 <73079502+xuwil1@users.noreply.github.com>
2022-05-14 21:00:19 -0400

    Create Beginner Series_3_Sum_of_Numbers.js

---
+++ 7th Kyu/Beginner Series_3_Sum_of_Numbers.js
@@ -0,0 +1,30 @@
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+    (1, 0) --> 1 (1 + 0 = 1)
+    (1, 2) --> 3 (1 + 2 = 3)
+    (0, 1) --> 1 (0 + 1 = 1)
+    (1, 1) --> 1 (1 since both are same)
+    (-1, 0) --> -1 (-1 + 0 = -1)
+    (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum( a,b )
+{
+  let total=0;
+  if(a===b){
+    total= a;
+  }else if(a<b){
+      for(let i=a;i<=b;i++){
+        total+=i
+      }
+  }else{
+      for(let i=b;i<=a;i++){
+        total+=i
+      }
+  }
+  return total;
+}
