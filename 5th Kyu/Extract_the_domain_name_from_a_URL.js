503bddac7176cf358910c9777089ce518f5a9b94
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-06-14 11:20:40 -0400

    Create Extract_the_domain_name_from_a_URL.js

---
+++ 5th Kyu/Extract_the_domain_name_from_a_URL.js
@@ -0,0 +1,15 @@
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+*/
+
+function domainName(url){
+  url=url.replace(/(.*?:\/\/)/,'');
+  url=url.replace(/w*\.*/,'');
+  url=url.replace(/((\..*))/,'');
+  return url;
+
+}
