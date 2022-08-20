59ec06b92ca90b02a439587e3f7caaac2d45257b
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-08-20 13:11:33 -0400

    Update Fix_your_code_before_the_garden_dies.js

--- 8th Kyu/Fix_your_code_before_the_garden_dies.js
+++ 8th Kyu/Fix_your_code_before_the_garden_dies.js
@@ -5,7 +5,7 @@ Your task is to debug the code before your plants die!
 */
 
 function rainAmount(mm){
-    if (mm <= 40) {
+    if (mm < 40) {
          return "You need to give your plant " + (40-mm) + "mm of water";
     }else {
          return "Your plant has had more than enough water for today!";
