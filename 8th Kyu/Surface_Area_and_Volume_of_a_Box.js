626889b37f14dcce1c39a8b2ddc111e66cb75453
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-08-29 01:07:46 -0400

    Create Surface_Area_and_Volume_of_a_Box.js

---
+++ 8th Kyu/Surface_Area_and_Volume_of_a_Box.js
@@ -0,0 +1,7 @@
+/*
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+*/
+
+function getSize(width, height, depth){
+  return[ 2*width*height + 2*width*depth+2*height*depth, width * height * depth]
+}
