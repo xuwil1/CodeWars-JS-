e839975c1d3a85bc59a4897002dc66821f970e90
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-12-08 12:37:43 -0500

    Create Exclamation_marks_series_6_Remove_n_exclamation_marks_in_the_sentence_from_left_to_right.js

---
+++ 8th Kyu/Exclamation_marks_series_6_Remove_n_exclamation_marks_in_the_sentence_from_left_to_right.js
@@ -0,0 +1,27 @@
+/*
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+function remove(s,n){
+  let temp="";
+  for(let c of s){
+    if(c=="!"&& n>0){
+      n--;
+      continue;
+    }
+      temp+= c;
+  }
+  return temp;
+}
