5554a9c7d8a50129aa2824f0539eae58b2675e63
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2023-01-24 12:04:45 -0500

    Create Convert_to_Binary.js

---
+++ 8th Kyu/Convert_to_Binary.js
@@ -0,0 +1,23 @@
+/*
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+//should return 1
+to_binary(1)  
+//should return 101
+to_binary(5) 
+//should return 1011
+to_binary(11)
+
+Example:
+
+//should return 1
+to_binary(1)  
+//should return 101
+to_binary(5) 
+//should return 1011
+to_binary(11)
+*/
+
+function toBinary(n){
+  return parseInt(n.toString(2));
+}
