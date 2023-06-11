e9e70e5ec1745f42c037109a118cc9438b2055d1
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2023-06-11 13:01:08 -0400

    Create Digits_explosion.js

---
+++ 7th Kyu/Digits_explosion.js
@@ -0,0 +1,24 @@
+/*
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+should return :
+
+"333122"
+explode("102269")
+should return :
+
+"12222666666999999999"
+*/
+
+function explode(s) {
+  s=s.split('');
+  let temp= '';
+  for(const x of s){
+    for(let i=x;i>0;i--){
+      temp+=x;
+    }
+  }
+  return temp;
+}
